b6f501de390722a4015bb994f079b71d626b46ce
SAJID LATIF <indus1530@gmail.com>
2020-03-05 13:53:38 +0500

    some changess

--- app/src/androidTest/java/edu/aku/hassannaqvi/ctsam/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/aku/hassannaqvi/ctsam/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("edu.aku.hassannaqvi.uen_tmk", appContext.getPackageName());
+        assertEquals("edu.aku.hassannaqvi.ctsam", appContext.getPackageName());
     }
 }
