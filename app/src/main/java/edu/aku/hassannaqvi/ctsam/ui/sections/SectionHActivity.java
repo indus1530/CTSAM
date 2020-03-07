5a8fff2b631218f13c45aa2d74e86c0fadbc5cce
muhammad.farooqui <waqarfarooqui.12@gmail.com>
2020-03-07 11:32:26 +0500

    fixed skip in section H.

--- app/src/main/java/edu/aku/hassannaqvi/ctsam/ui/sections/SectionHActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/ctsam/ui/sections/SectionHActivity.java
@@ -7,6 +7,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import com.validatorcrawler.aliazaz.Clear;
 import com.validatorcrawler.aliazaz.Validator;
 
 import org.json.JSONException;
@@ -26,6 +27,83 @@ public class SectionHActivity extends AppCompatActivity {
         bi = DataBindingUtil.setContentView(this, R.layout.activity_section_h);
         bi.setCallback(this);
 
+        setlistener();
+
+    }
+
+    private void setlistener() {
+
+        bi.s8q1.setOnCheckedChangeListener(((radioGroup, i) -> {
+
+            if (i == bi.s8q102.getId()) {
+                Clear.clearAllFields(bi.fldGrpCVs8q1a1);
+            }
+
+        }));
+
+        bi.s8q2.setOnCheckedChangeListener(((radioGroup, i) -> {
+
+            if (i == bi.s8q202.getId()) {
+                Clear.clearAllFields(bi.fldGrpCVs8q2a1);
+            }
+
+        }));
+
+        bi.s8q3.setOnCheckedChangeListener(((radioGroup, i) -> {
+
+            if (i == bi.s8q302.getId()) {
+                Clear.clearAllFields(bi.fldGrpCVs8q3a1);
+            }
+
+        }));
+
+        bi.s8q4.setOnCheckedChangeListener(((radioGroup, i) -> {
+
+            if (i == bi.s8q402.getId()) {
+                Clear.clearAllFields(bi.fldGrpCVs8q4a1);
+            }
+
+        }));
+
+        bi.s8q5.setOnCheckedChangeListener(((radioGroup, i) -> {
+
+            if (i == bi.s8q502.getId()) {
+                Clear.clearAllFields(bi.fldGrpCVs8q5a1);
+            }
+
+        }));
+
+        bi.s8q6.setOnCheckedChangeListener(((radioGroup, i) -> {
+
+            if (i == bi.s8q602.getId()) {
+                Clear.clearAllFields(bi.fldGrpCVs8q6a1);
+            }
+
+        }));
+
+        bi.s8q7.setOnCheckedChangeListener(((radioGroup, i) -> {
+
+            if (i == bi.s8q702.getId()) {
+                Clear.clearAllFields(bi.fldGrpCVs8q7a1);
+            }
+
+        }));
+
+        bi.s8q8.setOnCheckedChangeListener(((radioGroup, i) -> {
+
+            if (i == bi.s8q802.getId()) {
+                Clear.clearAllFields(bi.fldGrpCVs8q8a1);
+            }
+
+        }));
+
+        bi.s8q9.setOnCheckedChangeListener(((radioGroup, i) -> {
+
+            if (i == bi.s8q902.getId()) {
+                Clear.clearAllFields(bi.fldGrpCVs8q9a1);
+            }
+
+        }));
 
     }
 
@@ -71,78 +149,95 @@ public class SectionHActivity extends AppCompatActivity {
                 bi.s8q101.isChecked() ? "1" :
                         bi.s8q102.isChecked() ? "2" :
                                 "0");
+
         json.put("s8q1a1",
                 bi.s8q1a101.isChecked() ? "1" :
                         bi.s8q1a102.isChecked() ? "2" :
                                 bi.s8q1a103.isChecked() ? "3" :
                                         "0");
+
         json.put("s8q2",
                 bi.s8q201.isChecked() ? "1" :
                         bi.s8q202.isChecked() ? "2" :
                                 "0");
+
         json.put("s8q2a1",
                 bi.s8q2a101.isChecked() ? "1" :
                         bi.s8q2a102.isChecked() ? "2" :
                                 bi.s8q2a103.isChecked() ? "3" :
                                         "0");
+
         json.put("s8q3",
                 bi.s8q301.isChecked() ? "1" :
                         bi.s8q302.isChecked() ? "2" :
                                 "0");
+
         json.put("s8q3a1",
                 bi.s8q3a101.isChecked() ? "1" :
                         bi.s8q3a102.isChecked() ? "2" :
                                 bi.s8q3a103.isChecked() ? "3" :
                                         "0");
+
         json.put("s8q4",
                 bi.s8q401.isChecked() ? "1" :
                         bi.s8q402.isChecked() ? "2" :
                                 "0");
+
         json.put("s8q4a1",
                 bi.s8q4a101.isChecked() ? "1" :
                         bi.s8q4a102.isChecked() ? "2" :
                                 bi.s8q4a103.isChecked() ? "3" :
                                         "0");
+
         json.put("s8q5",
                 bi.s8q501.isChecked() ? "1" :
                         bi.s8q502.isChecked() ? "2" :
                                 "0");
+
         json.put("s8q5a1",
                 bi.s8q5a101.isChecked() ? "1" :
                         bi.s8q5a102.isChecked() ? "2" :
                                 bi.s8q5a103.isChecked() ? "3" :
                                         "0");
+
         json.put("s8q6",
                 bi.s8q601.isChecked() ? "1" :
                         bi.s8q602.isChecked() ? "2" :
                                 "0");
+
         json.put("s8q6a1",
                 bi.s8q6a101.isChecked() ? "1" :
                         bi.s8q6a102.isChecked() ? "2" :
                                 bi.s8q6a103.isChecked() ? "3" :
                                         "0");
+
         json.put("s8q7",
                 bi.s8q701.isChecked() ? "1" :
                         bi.s8q702.isChecked() ? "2" :
                                 "0");
+
         json.put("s8q7a1",
                 bi.s8q7a101.isChecked() ? "1" :
                         bi.s8q7a102.isChecked() ? "2" :
                                 bi.s8q7a103.isChecked() ? "3" :
                                         "0");
+
         json.put("s8q8",
                 bi.s8q801.isChecked() ? "1" :
                         bi.s8q802.isChecked() ? "2" :
                                 "0");
+
         json.put("s8q8a1",
                 bi.s8q8a101.isChecked() ? "1" :
                         bi.s8q8a102.isChecked() ? "2" :
                                 bi.s8q8a103.isChecked() ? "3" :
                                         "0");
+
         json.put("s8q9",
                 bi.s8q901.isChecked() ? "1" :
                         bi.s8q902.isChecked() ? "2" :
                                 "0");
+
         json.put("s8q9a1",
                 bi.s8q9a101.isChecked() ? "1" :
                         bi.s8q9a102.isChecked() ? "2" :
