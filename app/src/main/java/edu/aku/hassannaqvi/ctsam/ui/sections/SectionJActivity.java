fad49e36589e9565a886ca37c85c81109553f61c
wasimm <wasim_40@yahoo.com>
2020-06-11 16:26:45 +0500

    commit changes done

--- app/src/main/java/edu/aku/hassannaqvi/ctsam/ui/sections/SectionJActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/ctsam/ui/sections/SectionJActivity.java
@@ -135,7 +135,7 @@ public class SectionJActivity extends AppCompatActivity {
                 : bi.s10q1b.isChecked() ? "2"
                 : bi.s10q1c.isChecked() ? "3"
                 : bi.s10q1d.isChecked() ? "4"
-                : "1");
+                : "0");
 
         json.put("s10q1ax", bi.s10q1ax.getText().toString());
         json.put("s10q1bx", bi.s10q1bx.getText().toString());
