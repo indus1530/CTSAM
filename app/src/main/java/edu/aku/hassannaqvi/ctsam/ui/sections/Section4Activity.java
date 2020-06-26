980bd8ed849d787062741c4ae95451f219e0f03a
wasimm <wasim_40@yahoo.com>
2020-06-26 10:12:26 +0500

    commit child follow up activities added

---
+++ app/src/main/java/edu/aku/hassannaqvi/ctsam/ui/sections/Section4Activity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.ctsam.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.ctsam.R;
+
+public class Section4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_4);
+    }
+}
---
+++ app/src/main/java/edu/aku/hassannaqvi/ctsam/ui/sections/Section6Activity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.ctsam.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.ctsam.R;
+
+public class Section6Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_6);
+    }
+}
